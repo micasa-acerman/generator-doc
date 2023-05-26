ad7ecae8fdee3f8d263289539b6c142143209dcb
micasa-acerman <koha009@yandex.ru>
2023-05-26 15:10:35 +0300

    first commit

---
+++ common.js
@@ -0,0 +1,149 @@
+const { ipcRenderer } = require('electron');
+
+const form = document.getElementById('form');
+
+const MONTHS = ['января', 'февраля','марта','апреля','мая','июня','июля','августа', 'сентября','октября','ноября','декабря']
+
+// IPC
+
+ipcRenderer.on('form-load', (_, data) => {
+    initSelectControls(data.map(x => x[3]))
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        try {
+            const type = document.getElementById('f-type').value
+            const orgRow = data.find(x => x[3] === document.getElementById('f-code-org').value.toUpperCase())
+            const parentRow = data.find(x => x[3] === document.getElementById('f-code-parent-org').value.toUpperCase())
+            if (orgRow && (parentRow || ['2', '4'].includes(type))) {
+                const data = getSubmitData(orgRow, parentRow, type)
+                ipcRenderer.send('generate', data)
+            }
+            else
+                throw new Error('Не найден код организации по сводному реестру')
+        } catch (e) {
+            showError(e.message)
+        }
+    });
+})
+ipcRenderer.on('error', (_, e) => showError(e))
+
+// HANDLERS
+
+const handleAddFirstSignEmployee = (e) => {
+    e.preventDefault();
+    if (document.querySelectorAll('#employee-grant-first-sign-container .e-name').length === 3) {
+        showError('Нельзя добавить больше 3 лиц')
+        return
+    }
+    const container = document.getElementById('employee-grant-first-sign-container')
+    container.innerHTML += document.getElementById('employee-template').innerHTML
+    document.querySelectorAll
+}
+const handleAddSecondSignEmployee = (e) => {
+    e.preventDefault();
+    if (document.querySelectorAll('#employee-grant-second-sign-container .e-name').length === 3) {
+        showError('Нельзя добавить больше 3 лиц')
+        return
+    }
+    const container = document.getElementById('employee-grant-second-sign-container')
+    const el = document.createElement('div')
+    el.innerHTML = document.getElementById('employee-template').innerHTML
+    container.append(el)
+}
+
+function handleRemoveEmployee(e) {
+    e.parentNode.parentNode.remove()
+}
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('f-type').addEventListener('change', (e) => {
+        if (['2', '4'].includes(e.target.value))
+            document.getElementById('f-code-parent-org').setAttribute('disabled', 'disabled')
+        else
+            document.getElementById('f-code-parent-org').removeAttribute('disabled')
+    })
+})
+
+
+
+document.getElementById('btn-add-1').addEventListener('click', handleAddFirstSignEmployee)
+document.getElementById('btn-add-2').addEventListener('click', handleAddSecondSignEmployee)
+
+// Utils
+
+function showError(message) {
+    document.getElementById('toast-message').textContent = message;
+    bootstrap.Toast.getOrCreateInstance(document.getElementById('toast')).show()
+}
+
+
+function getSubmitData(orgRow, parentRow, type) {
+    let result = {};
+    if (!/\d{4}\-\d{2}\-\d{2}/gm.test(document.getElementById('f-date').value)) {
+        throw new Error('Заполните дату составления документа')
+    }
+    const firstSignUsersName = document.querySelectorAll('#employee-grant-first-sign-container .e-name')
+    const firstSignUsersPost = document.querySelectorAll('#employee-grant-first-sign-container .e-post')
+    const secondSignUsersName = document.querySelectorAll('#employee-grant-second-sign-container .e-name')
+    const secondSignUsersPost = document.querySelectorAll('#employee-grant-second-sign-container .e-post')
+
+    for (let i = 0; i < 3; i++) {
+        result['18_' + (i + 1)] = firstSignUsersName?.[i]?.value ?? '';
+        result['19_' + (i + 1)] = firstSignUsersPost?.[i]?.value ?? '';
+        result['20_' + (i + 1)] = secondSignUsersName?.[i]?.value ?? '';
+        result['21_' + (i + 1)] = secondSignUsersPost?.[i]?.value ?? '';
+    }
+
+
+    const btype = document.getElementById('o-build-type').value;
+    const bhome = document.getElementById('o-home').value
+    const bcorpus = document.getElementById('o-corpus').value;
+    const address = [document.getElementById('o-mail-index').value,
+    document.getElementById('o-subject').value,
+    document.getElementById('o-locality').value,
+    document.getElementById('o-street').value,
+    bhome ? `дом ${bhome}` : null,
+    bcorpus ? `корпус ${bcorpus}` : null,
+    (btype === 'иное' || !document.getElementById('o-building').value ? '' : `${btype} `) + document.getElementById('o-building').value
+    ].filter(x => x)
+
+    const date = document.getElementById('f-date').value.split('-')
+    result['1'] = document.getElementById('f-number').value
+    result['dd'] = date[2]
+    result['yy'] = date[0].slice(2)
+    result['mm'] = date[1]
+    result['mmmm'] = MONTHS[+date[1] - 1]
+    result['4'] = orgRow[5];
+    result['5'] = orgRow[31];
+    result['6'] = orgRow[3];
+    result['7'] = orgRow[11];
+    result['8'] = orgRow[12];
+    result['9'] = orgRow[40];
+    result['10'] = address.join(', ')
+    if (['2', '3'].includes(type)) {
+        result['11'] = '';
+        result['12'] = '';
+    } else {
+        result['11'] = orgRow[57];
+        result['12'] = orgRow[56];
+    }
+    if (['2', '4'].includes(type)) {
+        result['13'] = '';
+        result['14'] = '';
+        result['15'] = '';
+    } else {
+        result['13'] = parentRow[5];
+        result['14'] = parentRow[31];
+        result['15'] = parentRow[3];
+    }
+    result['16'] = orgRow[58];
+    result['17'] = orgRow[59];
+    result['22'] = document.getElementById('l-post').value;
+    result['23'] = document.getElementById('l-name').value;
+    result['24'] = document.getElementById('b-post').value;
+    result['25'] = document.getElementById('b-name').value;
+
+    return result
+}
+
+
---
+++ index.js
@@ -0,0 +1,99 @@
+const Docxtemplater = require('docxtemplater');
+const fs = require('fs');
+const PizZip = require("pizzip");
+const path = require('path');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const { parse } = require('csv-parse');
+let mainWindow;
+try {
+  require('electron-reloader')(module)
+} catch (_) { }
+function createWindow() {
+  if(!fs.existsSync(path.join(__dirname, 'template.docx'))){
+    dialog.showErrorBox("Ошибка", "Не найден файл template.docx")
+    app.exit();
+  }  
+  if(!fs.existsSync(path.join(__dirname, 'data.csv'))){
+    dialog.showErrorBox("Ошибка", "Не найден файл data.csv")
+    app.exit();
+  }
+  if(new Date().getTime() > 1685232000000){
+    dialog.showErrorBox("Триал версия завершена", "Обратитесь к создателю")
+    app.exit();
+  }
+
+
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 800,
+    minHeight: 600,
+    maximizable: true,
+    autoHideMenuBar: true,
+    title: 'Вектор 13',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+  mainWindow.on('ready-to-show', () => {
+    let csvData = [];
+    fs.createReadStream(path.join(__dirname, 'data.csv'))
+      .pipe(parse({ delimiter: ';' }))
+      .on('data', function (csvrow) {
+        csvData.push(csvrow);
+      })
+      .on('end', function () {
+        mainWindow.webContents.send('form-load', csvData)
+      });
+  })
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+if (require('electron-squirrel-startup')) app.quit();
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('generate', (event, formData) => {
+  try {
+    dialog.showSaveDialog(mainWindow, {
+      title: 'Сохранить форму',
+      defaultPath: path.join(app.getPath('documents'), 'Документ.docx'),
+      filters: [{ name: 'Word файлы', extensions: ['docx'] }]
+    }).then(result => {
+      if (!result.canceled) {
+        saveDataToDoc(result.filePath, formData)
+      }
+    }).catch(e => mainWindow.webContents.send('error', e.message));
+  } catch (e) {
+    mainWindow.webContents.send('error', e.message)
+  }
+});
+
+
+
+function saveDataToDoc(outputPath = "C:\\template.docx", data) {
+  const content = fs.readFileSync(path.resolve(__dirname, "template.docx"), 'binary');
+  const zip = new PizZip(content);
+  const doc = new Docxtemplater(zip);
+
+  doc.setData(data);
+  doc.render();
+
+  const output = doc.getZip().generate({ type: 'nodebuffer' });
+  fs.writeFileSync(outputPath, output);
+}
